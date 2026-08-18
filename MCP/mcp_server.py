c0bf16a0cea0a27f4da5ee97c882543df4471a34
SarthakSuthar <sarthaksuthar2804@gmail.com>
2026-08-18 11:39:09 +0530

    divide tool added in server

--- MCP/mcp_server.py
+++ MCP/mcp_server.py
@@ -15,5 +15,16 @@ def multiply(a: float, b: float) -> float:
     return a * b
 
 
+@mcp.tool
+def divide(a: float, b: float) -> str:
+    """Divide the first number by the second.
+    Includes error handling for division by zero.
+    Use for division operation.
+    """
+    if b == 0:
+        return "Zero can not be divided"
+    return str(a / b)
+
+
 if __name__ == "__main__":
     mcp.run(transport="stdio")
